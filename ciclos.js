1ddd1af7152e9eca7af89213c7a4d5d65355ad96
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:08:22 -0400

    ciclos js

---
+++ ciclos.js
@@ -0,0 +1,16 @@
+//CICLOS
+
+//CICLO FOR
+    //var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+    //for( i = 0; i <= dias.length -1 ; i++ ){
+        //document.write(dias[i] + "<br>");
+
+    //}
+//CICLO WHILE
+  var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+  var numeroDias = dias.length -1 ;
+  var i = 0;
+  while (i <= numeroDias) {
+    document.write(dias[i] + "<br>");
+    i++;
+  }
