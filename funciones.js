69c2e25137309dd26aa734e593ecfdcb18e031c3
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:29:49 -0400

    funciones anonimas

--- funciones.js
+++ funciones.js
@@ -3,11 +3,20 @@
 // }
 //   saludo(" Emanuel");
 
-function suma(num1, num2){
-    var num1 = num1;
-    var num2 = num2;
-
-    return num1 + num2;
-
-}
-    suma(10,20);
+// function suma(num1, num2){
+//     var num1 = num1;
+//     var num2 = num2;
+//
+//     return num1 + num2;
+//
+// }
+//     document.write( suma(10,20));
+    //FUNCIONES ANONIMAS
+    numeroMaximo = function (valor1 , valor2){
+      if(valor1 > valor2){
+          return valor1;
+      }else {
+        return valor2;
+      }
+    }
+  document.write("El numero Maximo es: " + numeroMaximo(2,6));
