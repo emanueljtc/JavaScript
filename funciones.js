35680d66e590ffb268eddf9523e88e656ab6a38f
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:08:56 -0400

    funciones js

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// function saludo(nombre) {
+//       document.write("Bienvenido al Sistema" + nombre);
+// }
+//   saludo(" Emanuel");
+
+function suma(num1, num2){
+    var num1 = num1;
+    var num2 = num2;
+
+    return num1 + num2;
+
+}
+    suma(10,20);
