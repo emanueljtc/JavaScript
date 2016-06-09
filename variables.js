4804f35390f19dc1f5e338b957d847d427f60c2b
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:09:54 -0400

    variables js

---
+++ variables.js
@@ -0,0 +1,11 @@
+//var nombre ="Emanuel";
+//alert(nombre);
+    var nombre;
+      nombre = prompt("Ingresa tu nombre", "Escribe tu Nombre");
+      alert(nombre);
+    var num1, num2, mult;
+      num1 = prompt("Ingrese primer valor");
+      num2 = prompt("Ingrese segundo valor");
+        mult = num1 * num2;
+        document.write(mult);
+    //console.log(nombre); // mostrar el valor de la variable en consola F12
