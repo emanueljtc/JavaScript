80716ada43ebbe678a520b05bdf8f42c7d13ff28
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:09:45 -0400

    tipos js

---
+++ tipos.js
@@ -0,0 +1,28 @@
+//TIPOS DE DATOS
+
+//NUMERICO
+  var edad = 22;
+//STRING
+  var nombre = "Emanuel";
+  var apellido = "Torres";
+  //document.write(nombre + " " + apellido);
+  var texto = "Salvar \"Texto\"";
+  //alert(texto);
+//ARREGLOS
+  var amigos = ["Armando", "Miguel", "Santiago"];
+  //document.write(amigos[2]);
+//Objetos
+  var objeto = {
+    color = "Black",
+    size : 20
+  }
+//Boolean - Boleano
+  var boleano = false;
+//Undefined
+  var prueba;
+//Null
+  var num2 = 10;
+  var num2 = null; // Resetear variable
+//NaN - Valor invalido
+    var texto2 = "No puedo";
+    //document.write(texto2 * 2); // esto es invalido por lo tanto es NaN
