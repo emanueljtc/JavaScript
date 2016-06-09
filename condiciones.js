3f4bf190dd27d0093fad2f5f18df26cf1c325cf4
emanueljtc <emanueljtc@gmail.com>
2016-06-09 09:08:45 -0400

    condiciones js

---
+++ condiciones.js
@@ -0,0 +1,27 @@
+var nombre;
+
+    nombre = prompt("Ingresa tu Nombre");
+var edad;
+    edad =   prompt("Ingresa tu Edad");
+
+    // if(nombre == "Emanuel"){
+    //   document.write("Bienvenido " + nombre);
+    // }else if (nombre == "Ana"){
+    //     document.write("Bienvenido " + nombre);
+    //
+    //  }else {
+    //     document.write("Nombre no Reconocido");
+    //  }
+
+    // > mayor que
+    // < menor que
+    // >= mayor o igual
+    // <= menor o igual
+    // == igual que
+    // != distinto de
+
+        if(edad >= 18 && nombre == "Emanuel"){
+            document.write("Bienvenido, " + nombre +  " eres mayor de edad");
+        }else {
+          document.write("Tu no cumples");
+        }
