efc76814ff63f1a25f35265cd7a9670d84af0629
emanueljtc <emanueljtc@gmail.com>
2016-06-09 21:52:07 -0400

    calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+var suma = function(num1, num2) {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+
+  var resultado = num1 + num2;
+  return resultado;
+}
