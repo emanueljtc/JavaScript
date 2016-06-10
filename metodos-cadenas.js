c5d1e25909d5e25f32cbf125d9b3175aa78999ab
emanueljtc <emanueljtc@gmail.com>
2016-06-09 22:34:56 -0400

    metodos-cadenas.js

---
+++ metodos-cadenas.js
@@ -0,0 +1,12 @@
+var texto = "Emanuel Torres Clemente"
+//var nuevoTexto = texto.length;  //CUENTA LOS CARACTERES DE LA CADENA
+//var nuevoTexto = texto.substring(8); //CORTA LA CADENA
+//var nuevoTexto = texto.substr(0, 2); //CORTA LA CADENA POR POSICION
+//var texto2 = texto.indexOf("T") + 1;
+//var nuevoTexto = texto.indexOf("T", texto2); //NOS DEVUELVE POSICION DE UNA LETRA EN ESPECIFICO
+//var nuevoTexto = texto.lastIndexOf("e");  //DEVUELVE POSICION DE LETRA DE ATRAS HACIA ADELANTE
+//var nuevoTexto = texto.replace(texto, "Ana Cordova"); //REMPLAZA LA CADENA DE TEXTO
+var nuevoTexto = texto.toUpperCase(); //TEXTO MAYUSCULA
+var nuevoTexto = texto.toLowerCase(); //texto minuscula
+
+document.write(nuevoTexto);
